6170d0b0fe810522b2cd09af342bef69e84a6cfa
Gustavo Litovsky <gustavo@argenox.com>
2023-02-24 16:42:38 -0600

    Updates for parsing files

--- management/core/PageParser.py
+++ management/core/PageParser.py
@@ -2,7 +2,6 @@ import os
 from bs4 import BeautifulSoup
 import bs4
 
-
 from djangocms_site_importer.management.core.ElementParser import *
 from djangocms_site_importer.management.core.HeadingParser  import *
 from djangocms_site_importer.management.core.ParagraphParser import *
@@ -21,56 +20,90 @@ class PageParser:
         self.filepath = filepath
         self.page_name = ""
 
-    
+    def checkChildValid(self, child):
+        if(child is not None and child != "None" and child.name != None):
+            return True
+        else:
+            return False
+        
     def parseFile(self):
         with open(self.filepath) as fp:
-                soup = BeautifulSoup(fp, "html.parser")
+            soup = BeautifulSoup(fp, "html.parser")
 
-                if(soup is not None and soup.head is not None):
-                    self.page_name = soup.find_all('title')
-                    for child in soup.body:
-                        #print("Child: " + str(child.name) + "   " + str(child))
-                        self.parseSection(child, str(child.name))
+            if(soup is not None and soup.head is not None):
 
-    def parseSection(self, section, tag, parent=None):
+                # Extract Title
+                self.page_name = soup.find_all('title')[0].string.split('|', 1)[0]
+                new_page = self.createPage(self.page_name)
+                                    
+                if(new_page is not None):                        
+                    page_placeholder = new_page.placeholders.all()[0]
+                    self.processChildren(soup.body, new_page)
+                    
 
-        if(tag != None and tag != "None"):
-            print("Parsing Tag")
+    def processChildren(self, children, page, parent_plugin=None):
+        # Iterate over all items in 
+        for child in children:
+            if(self.checkChildValid(child)):
+                print("=======================================================================")
+                print("Child: Name: " + str(child.name))
+
+                from cms.api import add_plugin
+
+                tag = child.name
+
+                parser = None
+
+                if(tag != None and tag != "None"):            
+                    for p in self.parser_list:
+                        if(tag.startswith(p.getElementType())):
+                            
+                            print("Tag Starts with it " + p.getElementType())                            
+                            parser_object = type(p)
+
+                            klass = globals()[p.__name__]
+                            parser = klass(child, tag)
+
+                            if(parser is not None):
+
+                                print("Adding Plugin: " + parser.getPluginName())
+                                print("\t" + parser.getPluginBody())
+
+                                placeholder = page.placeholders.all()[0]
+                                parent = placeholder
+
+                                print("parent  " + str(parent))
+                                print("Page" + str(type(page)))
+                                print("Placeholder" + str(type(placeholder)))
+                                                                
+                                new_plug = parser.createPlugin(parent, parent_plugin)
+
+                                for sub_child in child:
+                                    if(self.checkChildValid(sub_child)):
+                                        print("\tSubchild Type:" + str(sub_child.name))                                       
+                                        if type(child) is not bs4.element.NavigableString:                                            
+                                            self.processChildren(sub_child, page, parent_plugin=new_plug)
+                                          
+        
+    def createPage(self, name):
+        from cms.api import create_page
+        new_page = create_page(name, language='en', template='bootstrap5.html')
+        return new_page
+
+    def getParser(self, section, tag):
+        if(tag != None and tag != "None"):            
             for p in self.parser_list:
-                
-                #print("THis: " + HeadingParser.getElementType())
-                #print("tag: " + tag)
-
-                #print("Does it start? " + str(tag.startswith(p.getElementType())))
                 if(tag.startswith(p.getElementType())):
                     
-                    print("Tag Starts with it " + p.getElementType())
-                    # instantiate it
-                    #print(p.getElementType())
+                    print("Tag Starts with it " + p.getElementType())                    
                     parser_object = type(p)
 
                     klass = globals()[p.__name__]
-                    instance = klass(section, tag)
-                    #instance.print()
-
-                    if(parent is None):
-                        #print("Top")
-                        self.parse_tree.append(instance)
-                    else:
-                        #print("Child of ")
-                        #parent.print()
-                        parent.children.append(instance)
-
-                    for child in section:
-                        #print("CHild Type:" + str(type(child)))
-                        #print("=================================================")
-                        #print(child)
-                        #print("=================================================")
-                        #if child != None and child.name != None:
-                        if type(child) is not bs4.element.NavigableString:
-                            print("Parsing  child  " + child.name)
-                            #print("parsing child in section" + str(child))
-                            #if(child.tag != "None"):
-                            #print("Child Name: " + child.name)
-                            #print("Child Tag: " + child.tag )
-                            self.parseSection(child, child.name, parent=instance)
+                    parserInstance = klass(section, tag)
+                    return parserInstance
+                else:
+                    print("No Parser")
+                    return None
+        else:
+            print("Error Getting parser")
+            return None
